62fab53841e76e00fe50408e67140f44327b12ce
Ayush Rudani <ayushcrudani@gmail.com>
2022-03-30 10:34:52 +0530

    Update

--- src/main/java/com/ec/controllers/MainController.java
+++ src/main/java/com/ec/controllers/MainController.java
@@ -64,6 +64,12 @@ public class MainController {
 	// }
 	// }
 
+	@ModelAttribute
+	public void Comman(Model model) {
+		int logIn = 0;
+		model.addAttribute("logIn", logIn);
+	}
+
 	@RequestMapping("/index")
 	public String showPage(HttpSession session, Model model) {
 		// if(session.getAttribute("fuser") == null) return "redirect:/signup";
@@ -108,6 +114,9 @@ public class MainController {
 		request.getSession().setMaxInactiveInterval(60 * 60);
 		request.getSession().setAttribute("fuser", foundUser);
 		model.addAttribute("user", foundUser);
+		int logIn = (int) model.getAttribute("logIn");
+		logIn = 1;
+		model.addAttribute("logIn", logIn);
 
 		// if (foundUser.getUser_type().equals("ADMIN") ||
 		// foundUser.getUser_type().equals("USER")) {
